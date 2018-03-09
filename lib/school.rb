46bd8e59c3135647d21021ef40f7092a31733efa
Kelsey Fecho <kfecho30@gmail.com>
2018-03-09 20:11:53 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,12 @@
 class School
   attr_reader :roster, :name
+
   def initialize(name)
     @name = name
     @roster = {}
   end
+
+  def add_student=(name, age)
+    @name = name
+    @age = age
 end
